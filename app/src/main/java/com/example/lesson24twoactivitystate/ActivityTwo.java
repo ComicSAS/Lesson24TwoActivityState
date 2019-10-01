0b2b7eb1575a863ddaeefe2c8afcde4f8d242d61
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-10-01 13:35:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson24twoactivitystate/ActivityTwo.java
@@ -0,0 +1,52 @@
+package com.example.lesson24twoactivitystate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    final String TAG = "LifeCycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        Log.d(TAG, "ActivityTwo: onCreate()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ActivityTwo: onResume()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "ActivityTwo: onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ActivityTwo: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ActivityTwo: onStop()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ActivityTwo: onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "ActivityTwo: onRestart()");
+    }
+}
